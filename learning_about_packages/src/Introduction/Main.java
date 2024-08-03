ddbe101e8cd12390b233596f22f3883922cf46c7
Abdul <malik.kasganj@gmail.com>
2024-08-03 16:05:50 +0530

    Packages

---
+++ learning_about_packages/src/Introduction/Main.java
@@ -0,0 +1,13 @@
+package Introduction;
+import static packages.b.Message.message;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("abdul malik");
+		message();
+
+	}
+
+}
---
+++ learning_about_packages/src/packages/b/Message.java
@@ -0,0 +1,12 @@
+package packages.b;
+
+public class Message {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void message() {
+		System.out.println("HOW ARE YOU DOING DEAR ");
+	}
+}
